3f1c69a2655a002ce274b10b257e7dce9d21dd8e
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2024-10-29 21:52:52 +0700

    feat: change navbar and sidebar design ui

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,17 +1,38 @@
 <aside
     id="logo-sidebar"
-    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    class="fixed top-0 bg-cover bg-center bg-dprimary left-0 z-40 w-64 h-screen pt-8 transition-transform -translate-x-full border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
     aria-label="Sidebar"
+    style="
+        background-image: url('http://iqacs-duck.research-ai.my.id/images/pattern.png');
+    "
 >
-    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <ul class="space-y-2 font-medium">
+    <div class="h-full pl-5 pr-5 overflow-y-auto dark:bg-gray-800">
+        <a class="flex items-center justify-center w-full" href="#">
+            <div class="flex-1 flex justify-center items-center">
+                <div>
+                    <img
+                        src="/images/logo.png"
+                        class="h-12 w-12 me-3"
+                        alt="FlowBite Logo"
+                    />
+                </div>
+                <div class="">
+                    <span
+                        class="block font-semibold text-lwhite transition-all text-base duration-200 mr-4"
+                    >
+                        IQACS - ITIK
+                    </span>
+                </div>
+            </div>
+        </a>
+        <ul class="space-y-2 font-medium text-white mt-8">
             <li>
                 <a
                     href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                    class="flex items-center p-2 rounded-lg dark:text-white hover:text-orange hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                     <i
-                        class="ti ti-file-analytics w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                        class="ti ti-file-analytics text-2xl text-gray-200 transition duration-75 dark:text-gray-400 group-hover:text-orange dark:group-hover:text-orange"
                     ></i>
                     <span class="ms-3">Dashboard</span>
                 </a>
