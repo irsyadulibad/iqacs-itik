0291557d0ce269b0aed36f49080050e112b3316d
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2024-10-30 01:11:40 +0700

    add: single php file for API

---
+++ public/temperature_data.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\DeviceValue;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle($request);
+
+$temp = DeviceValue::create([
+    'device_id' => $request->id_alat,
+    'type' => 'temperature',
+    'value' => $request->nilai,
+]);
+
+header('Content-Type: application/json');
+echo json_encode([
+    'status' => 'ok',
+    'message' => 'Success',
+]);
